c6b1e98c813fd24de20000333b10ce3a6b77d927
gui machiavelli <hey@guimachiavelli.com>
2015-03-07 01:28:29 +0000

    pdf generation process more or less done

--- generate-pdf.rb
+++ generate-pdf.rb
@@ -1,14 +1,20 @@
 require 'prawn'
 
 def insert_image(image_file, project_dir)
+    start_new_page
     image_file = image_file.gsub('img:', '').strip
     image_file = File.join(project_dir, 'images', image_file)
-    image(image_file, :fit => [500, 400]) unless File.extname(image_file) == ''
+    image(image_file, :fit => [525, 400]) unless File.extname(image_file) == ''
+    move_down Random.rand(250)
 end
 
 def insert_link(line)
     href = line.gsub('url:', '').strip
-    text "<link href='#{href}'>#{href}</link>", :inline_format => true
+    formatted_text [{
+        :text => href,
+        :link => href,
+        :styles => [:underline]
+    }]
 end
 
 def setup_font
@@ -22,7 +28,7 @@ end
 
 def generate_project_pdf(file, project_dir, project)
     filename = File.join('dist', project + '.pdf')
-    Prawn::Document.generate(filename) do
+    Prawn::Document.generate(filename, :page_size => 'A4') do
         content = File.new(file)
 
         setup_font
@@ -32,20 +38,12 @@ def generate_project_pdf(file, project_dir, project)
                 insert_image(line, project_dir)
             elsif line.start_with? 'url:'
                 insert_link(line)
-            elsif line.start_with? '##'
-                start_new_page
-                font_size(25) { text line }
             elsif line.start_with? '#'
-                font_size(60) { text line }
+                font_size(50) { text line.gsub('#', '') }
             else
                 text line
             end
-            #puts project if line.start_with? 'img:'
-            #puts 'image' if line.start_with? 'img:'
         end
-
-        #text(parse_md(content)) if File.extname(file) == '.md'
-        #start_new_page
     end
 end
 
@@ -64,4 +62,3 @@ def generate_pdfs
 end
 
 generate_pdfs
-
